84e38042ca4e03d0a0482ca0594142ff0f5334c3
Giampaolo Greco <grecog640@gmail.com>
2026-09-11 12:15:09 +0100

    first

---
+++ src/test/java/com/media/portal/developerportal/DeveloperPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media.portal.developerportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
