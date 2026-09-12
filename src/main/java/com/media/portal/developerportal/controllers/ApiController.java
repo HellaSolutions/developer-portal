e9c10a6cc0689c90f98af4936c5899f889f1ceec
Giampaolo Greco <grecog640@gmail.com>
2026-09-12 22:52:21 +0100

    nit

--- src/main/java/com/media/portal/developerportal/controllers/ApiController.java
+++ src/main/java/com/media/portal/developerportal/controllers/ApiController.java
@@ -56,7 +56,7 @@ public class ApiController {
         return ResponseEntity.ok(apis);
     }
 
-    @GetMapping("/{id}/")
+    @GetMapping("/{id}")
     public ResponseEntity<Api> getApi(@PathVariable Long id) {
         Api api = apiService.getApi(id);
         return ResponseEntity.ok(api);
